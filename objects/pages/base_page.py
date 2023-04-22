ebe3216e51c671105de3a9ec885ad1eca018fe81
Светлана Шараева <sharaeva_sveta@mail.ru>
2023-04-22 10:05:40 +0300

    add: добавила освные методы для работы с браузером

--- objects/pages/base_page.py
+++ objects/pages/base_page.py
@@ -1,27 +1,30 @@
+from time import sleep
+
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
 
-    def __int__(self, browser):
+    def __int__(self, browser, timeout: int = 15):
         self.browser = browser
+        self.browser.implicitly_wait(timeout)
         self.waiting_time = 15
 
-    def wait_for_element_present(self, loc, waiting_time: int):
+    def wait_for_element_present(self, loc: tuple, waiting_time: int):
 
         try:
             element = self.browser.WebDriverWait(waiting_time).until(EC.presence_of_element_located(*loc))
             return element
-        except Exception:
-            print("Element is absent")
+        except:
+            raise Exception("")
 
     def find_element(self, loc):
 
         try:
             elem = self.wait_for_element_present(*loc, waiting_time=self.waiting_time).find_element()
             return elem
-        except Exception:
-            print("Element is absent")
+        except:
+            raise Exception("")
 
     def send_value(self, loc, value):
 
@@ -30,3 +33,20 @@ class BasePage:
     def clicker(self, loc):
 
         self.find_element(loc).click()
+
+    def go_to_url(self, page_url):
+
+        try:
+            before_url = self.browser.current_url
+
+            if before_url == page_url:
+                self.browser.get('about:blank')
+                sleep(2)
+            self.browser.get(page_url)
+        except:
+            raise Exception("")
+
+
+
+
+
